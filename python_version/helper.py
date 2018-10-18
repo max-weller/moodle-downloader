d3524c7f3a295ebb36e6c824c23f5e07ded315f4
christianhauff <24371132+christianhauff@users.noreply.github.com>
2018-10-18 15:01:46 +0200

    Fixed helper.py work with Moodle of HS-Pforzheim
    Several css-selectors were different when using the script on the Moodle-Instance of HS-Pforzheim

--- python_version/helper.py
+++ python_version/helper.py
@@ -4,11 +4,11 @@ import getpass
 def login(url, username, password):
     browser = mechanicalsoup.Browser()
     page = browser.get(url)
-    link = page.soup.select('.loginpanel a:nth-of-type(1)')[0].attrs['href']
+    link = page.soup.select('.loginpanel form:nth-of-type(1)')[0].attrs['action']
     login_page = browser.get(link)
     link_domain = "/".join(login_page.url.split('/')[0:3])
-    form = login_page.soup.select('form#fm1')[0]
-    submit_url = link_domain + form.attrs['action']
+    form = login_page.soup.select('form#login')[0]
+    submit_url = form.attrs['action']
     form.select('#username')[0].attrs['value'] = username
     form.select('#password')[0].attrs['value'] = password
     page = browser.submit(form, submit_url)
