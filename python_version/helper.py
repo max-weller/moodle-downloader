e8575a803964dd92faa0cd8f13c7e938977309ff
Max <max@teamwiki.de>
2018-10-24 18:35:32 +0200

    fix login url bug

--- python_version/helper.py
+++ python_version/helper.py
@@ -1,14 +1,18 @@
 import mechanicalsoup
 import getpass
+from urllib.parse import urljoin
 
 def login(url, username, password):
     browser = mechanicalsoup.Browser()
     page = browser.get(url)
     link = page.soup.select('.loginpanel a:nth-of-type(1)')[0].attrs['href']
     login_page = browser.get(link)
-    link_domain = "/".join(login_page.url.split('/')[0:3])
+    print("login_page.url",login_page.url)
+    print("link",link)
     form = login_page.soup.select('form#fm1')[0]
-    submit_url = link_domain + form.attrs['action']
+    submit_url = urljoin(login_page.url, form.attrs['action'])
+    print("submit_url",submit_url)
+    print("form action",form.attrs['action'])
     form.select('#username')[0].attrs['value'] = username
     form.select('#password')[0].attrs['value'] = password
     page = browser.submit(form, submit_url)
